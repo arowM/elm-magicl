0249e28e28065ef171e9e6eb0531ae002da3d303
Kadzuya Okamoto <arow.okamoto@gmail.com>
2017-03-07 12:04:47 +0900

    Change to handle state by `data-magicl-state` attribute value.

--- src/Magicl.elm
+++ src/Magicl.elm
@@ -41,7 +41,7 @@ type Magicl msg state
   = Magicl
     { tagName : String
     , attributes : List (Attribute msg)
-    , css : List (String -> Css.Mixin)
+    , css : List (String -> Css.Snippet)
     , children : List (Magicl msg ())
     }
 
@@ -51,13 +51,13 @@ type Magicl msg state
 compile : String -> Magicl msg state -> ( Html msg, Css.Stylesheet )
 compile namespace magicl =
   let
-    ( html, snippet ) =
+    ( html, snippets ) =
       compile_ namespace <| coerce magicl
   in
-    ( html, Css.stylesheet [ snippet ] )
+    ( html, Css.stylesheet snippets )
 
 
-compile_ : String -> Magicl msg () -> ( Html msg, Css.Snippet )
+compile_ : String -> Magicl msg () -> ( Html msg, List Css.Snippet )
 compile_ id (Magicl { tagName, attributes, css, children }) =
   let
     ( _, htmls_, snippets_ ) =
@@ -72,15 +72,13 @@ compile_ id (Magicl { tagName, attributes, css, children }) =
         ( html, snippet ) =
           compile_ id_ magicl
       in
-        ( n + 1, html :: htmls, snippet :: snippets )
+        ( n + 1, html :: htmls, snippet ++ snippets )
   in
     ( Html.node
       tagName
       (Attributes.class id :: attributes)
       htmls_
-    , Css.class id <|
-      Css.children snippets_
-        :: List.map (\f -> f id) css
+    , snippets_
     )
 
 
@@ -98,21 +96,22 @@ empty =
 
 setStyle : List Css.Mixin -> Magicl msg state -> Magicl msg state
 setStyle styles =
-  Lens.modify css (\ls -> (List.map always styles) ++ ls)
+  Lens.modify css (\ls -> (\id -> Css.class id styles) :: ls)
 
 
 setStyleOn : state -> List Css.Mixin -> Magicl msg state -> Magicl msg state
 setStyleOn state styles =
   let
-    css_ : String -> Css.Mixin
+    css_ : String -> Css.Snippet
     css_ id =
-      Css.withClass (id ++ "-" ++ toString state) styles
+      Css.selector ("." ++ id ++ "[data-magicl-state=\"" ++ toString state ++ "\"]") styles
   in
     Lens.modify css (\ls -> css_ :: ls)
 
 
 coerce : Magicl msg a -> Magicl msg b
-coerce (Magicl o) = Magicl o
+coerce (Magicl o) =
+  Magicl o
 
 
 
@@ -151,7 +150,7 @@ attributes =
 
 {-| Lens for CSS.
 -}
-css : Lens (Magicl msg state) (List (String -> Css.Mixin))
+css : Lens (Magicl msg state) (List (String -> Css.Snippet))
 css =
   let
     get (Magicl magicl) =
